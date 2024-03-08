6776154125b3d91fe5dab5cc174079c1058c73a6
= <mukhtarrahimi749@gmail.com>
2024-03-08 11:52:13 +0430

    the react js code add to project

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -2,18 +2,24 @@ import "./LoginSignup.css";
 import user_icon from "../Assets/user.png";
 import email_icon from "../Assets/email.png";
 import password_icon from "../Assets/password.png";
+import { useState } from "react";
 const LoginSignup = () => {
+  const [action, setAction] = useState("Sign Up");
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
-          <img src={user_icon} alt="" />
-          <input type="text" placeholder="Name" />
-        </div>
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>
+        )}
 
         <div className="input">
           <img src={email_icon} alt="" />
@@ -24,14 +30,21 @@ const LoginSignup = () => {
           <input type="password" placeholder="Password" />
         </div>
       </div>
-
-      <div className="forgot-password">
-        Lost password? <span>Click Here!</span>
-      </div>
+      {action === "Sign Up" ? (
+        <div></div>
+      ) : (
+        <div className="forgot-password">
+          Lost password? <span>Click Here!</span>
+        </div>
+      )}
 
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() =>{setAction('Sign Up')}}>
+          Sign Up
+        </div>
+        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={()=>{setAction('Login')}}>
+          Login
+        </div>
       </div>
     </div>
   );
